f630be90ee6b4f930ca1486c1560a72443c03588
Iman Tabrizian <Tabrizian@outlook.com>
2017-08-25 11:03:30 +0430

    Improves mixin and global registration

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-import mqtt from 'mqttsocket'
+import Mqtt from 'lib/mqtt'
 
 export default {
   install(Vue, options) {
+    Vue.mixin({
+      async created: () => {
+        const client = await Mqtt.connect(options)
+        Vue.prototype.$mqtt = client
+      }
+    })
 
   }
 }
